fadc07cd3e1f4c11b61fe00cc5cd9c258b3c59f3
Jay <jaypee.1797@gmail.com>
2016-11-09 15:00:49 -0800

    Added Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :comments
+  resources :posts
+  resources :subreddits
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
