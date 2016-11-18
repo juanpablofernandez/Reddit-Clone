978c33095dcadbc4e9d500fd84cc6d39c1510f68
Jay <jaypee.1797@gmail.com>
2016-11-18 15:17:38 -0800

    New Posts now show on index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+      @posts = Post.all
   end
 
   def new
@@ -7,5 +8,29 @@ class PostsController < ApplicationController
   end
 
   def create
+      @post = Post.new(post_params)
+      if @post.save
+          redirect_to root_path
+      end
+  end
+
+  def show
+      @post = Post.find(params[:id])
+  end
+
+  def edit
+      @post = Post.find(params[:id])
+  end
+  def update
+      @post = Post.find(params[:id])
+      @post.update(title:params[:title], body: params[:body])
+      @post.save 
+      redirect_to root_path
+  end
+
+
+  private
+  def post_params
+      params.require(:post).permit(:title, :body, :url)
   end
 end
